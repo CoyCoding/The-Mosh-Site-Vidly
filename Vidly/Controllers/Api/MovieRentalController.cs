62cc260d4c821c2a6629e2f57b4306b590b821a3
matt coy <fluffytoycoy@gmail.com>
2018-10-22 19:38:13 -0500

    Added movie rental api and controller for getting rentals and posting rentals
    Added New Rental to nav bar

--- Vidly/Controllers/Api/MovieRentalController.cs
+++ Vidly/Controllers/Api/MovieRentalController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -18,42 +19,55 @@ namespace Vidly.Controllers.Api
             _db = new ApplicationDbContext();
         }
 
+
+        public IHttpActionResult GetMovieRentals()
+        {
+            var movieRentals = _db.RentalInfos.Include(ri => ri.Movie).Include(ri => ri.Customer).ToList();
+
+            return Ok(movieRentals);
+        }
+
         [HttpPost]
         public IHttpActionResult CreateMovieRental(MovieRentalDto movieRental)
         {
             var customer = _db.Customers.SingleOrDefault(c => c.Id == movieRental.CustomerId);
 
-            if(customer == null)
+            #region BadRequest CustomerID
+            if (customer == null)
             {
-                return BadRequest("Invaild customer Id");
+                return BadRequest("Invalid CustomerId");
             }
+            #endregion
 
-            if(movieRental.MovieIds.Count == 0)
+            #region BadRequest No Movies In MovieRental
+            if (movieRental.MovieIds.Count == 0)
             {
-                return BadRequest("No Movie Ids");
+                return BadRequest("No movies selected");
             }
+            #endregion
 
             var movies = _db.Movies.Where(m => movieRental.MovieIds.Contains(m.Id)).ToList();
 
-            if(movies.Count != movieRental.MovieIds.Count)
+            #region BadRequest Invalid MovieId in MovieRental
+            if(movieRental.MovieIds.Count != movies.Count)
             {
-                return BadRequest("One of the movie Ids was bad");
+                BadRequest("One or more invalid MovieIds");
             }
-
-            if(movies == null)
+            #endregion
 
             foreach (var movie in movies)
             {
-                    if(movie.QuantityAvailable == 0)
-                    {
-                        return BadRequest("Movie is not available");
-                    }
+                #region BadRequest Movie is out of stock
+                if (movie.QuantityAvailable <= 0)
+                {
+                    BadRequest("Movie is out of stock");
+                }
+                #endregion
 
                 --movie.QuantityAvailable;
 
                 var movieToRent = new RentalInfo
                 {
-                    
                     Customer = customer,
                     Movie = movie,
                     DateRented = DateTime.Now
